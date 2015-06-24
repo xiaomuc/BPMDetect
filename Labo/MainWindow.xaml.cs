83600386def06c4ed17de0f193e2cea6e5e17445
xiaomuc <xkogij@gmail.com>
2015-06-24 17:20:49 +0900

    お試しで波形みてみたけど。

--- Labo/MainWindow.xaml.cs
+++ Labo/MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 using iTunesLib;
 using SoundAnalyzeLib;
+using CSCore;
+using CSCore.Codecs;
+using CSCore.Streams;
 
 namespace Labo
 {
@@ -29,18 +32,52 @@ namespace Labo
             InitializeComponent();
             _ituneApp = new iTunesApp();
             WrapCollection collection = new WrapCollection(_ituneApp.LibraryPlaylist.Tracks);
-            
+
             lvTracks.ItemsSource = collection;
         }
 
         private void lvTracks_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            WrapTrack wt= lvTracks.SelectedItem as WrapTrack;
+            WrapTrack wt = lvTracks.SelectedItem as WrapTrack;
             wt.DetectedBPM = 100;
 
             IITFileOrCDTrack track = wt.Track as IITFileOrCDTrack;
             CsvWriter writer = CsvWriter.getInstance();
             writer.WaveSourceToCsv(track.Location, @"C:\Users\9500268\Desktop\Music\test.csv");
+        }
+
+        private void btnShowWave_Click(object sender, RoutedEventArgs e)
+        {
+            WrapTrack wt = lvTracks.SelectedItem as WrapTrack;
+            IITFileOrCDTrack track = wt.Track as IITFileOrCDTrack;
+            double fromSec = (double)iudFromSec.Value;
+            double toSec = (double)iudToSec.Value;
+            IWaveSource waveSource = CodecFactory.Instance.GetCodec(track.Location);
+            txWaveInfo.Text = string.Format("Rate:{0}\nChannels:{1}\nByte/Block:{2}\nByte/Sample:{3}",
+                waveSource.WaveFormat.SampleRate,
+                waveSource.WaveFormat.Channels,
+                waveSource.WaveFormat.BytesPerBlock,
+                waveSource.WaveFormat.BytesPerSample);
+            ISampleSource sampleSource = waveSource.ToSampleSource();
+            int sizeToRead = (int)(sampleSource.WaveFormat.SampleRate * (toSec - fromSec) * sampleSource.WaveFormat.Channels);
+            sampleSource.Position = (int)(sampleSource.WaveFormat.SampleRate * fromSec * sampleSource.WaveFormat.Channels);
+
+            int read = 0;
+            float[] buffer = new float[sampleSource.WaveFormat.Channels];
+            List<KeyValuePair<float, float>> left = new List<KeyValuePair<float, float>>();
+            List<KeyValuePair<float, float>> right = new List<KeyValuePair<float, float>>();
+            while ((read += sampleSource.Read(buffer, 0, sampleSource.WaveFormat.Channels)) < sizeToRead)
+            {
+                float pos = (float)sampleSource.Position / (float)(2 * sampleSource.WaveFormat.SampleRate);
+                left.Add(new KeyValuePair<float, float>(pos, buffer[0]));
+                if (sampleSource.WaveFormat.Channels > 1)
+                {
+                    right.Add(new KeyValuePair<float, float>(pos, buffer[1]));
+                }
+            }
+            
+            seriesLeft.ItemsSource = left;
+            seriesRight.ItemsSource = right;
         }
     }
 }
