015035212e42e15f7e773e321943481e6d39da5a
xiaomuc <xkogij@gmail.com>
2015-06-25 17:32:44 +0900

    LINQを使ってみた

--- Labo/MainWindow.xaml.cs
+++ Labo/MainWindow.xaml.cs
@@ -26,7 +26,11 @@ namespace Labo
     /// </summary>
     public partial class MainWindow : Window
     {
+        int MAX_SIZE = 33554432;
         iTunesApp _ituneApp;
+        List<float> leftList;
+        List<float> rightList;
+        ISampleSource _sampleSource;
         public MainWindow()
         {
             InitializeComponent();
@@ -39,45 +43,81 @@ namespace Labo
         private void lvTracks_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             WrapTrack wt = lvTracks.SelectedItem as WrapTrack;
-            wt.DetectedBPM = 100;
 
             IITFileOrCDTrack track = wt.Track as IITFileOrCDTrack;
-            CsvWriter writer = CsvWriter.getInstance();
-            writer.WaveSourceToCsv(track.Location, @"C:\Users\9500268\Desktop\Music\test.csv");
+            IWaveSource waveSource = CodecFactory.Instance.GetCodec(track.Location);
+            _sampleSource = waveSource.ToSampleSource();
+            txWaveInfo.Text = string.Format("{0}[Hz] Ch:{1}",
+                     waveSource.WaveFormat.SampleRate,
+                     waveSource.WaveFormat.Channels);
+            iudFrameCount.Value = (int)_sampleSource.Length / (int)iudFrameSize.Value;
+            btnShowWave.IsEnabled = true;
+        }
+        void showWave()
+        {
+            int frameSize = (int)iudFrameSize.Value;
+            int frameCount = (int)iudFrameCount.Value*2;
+            float[] buffer = new float[frameSize * frameCount];
+            float fromSec = (float)_sampleSource.GetPosition().TotalSeconds;
+            int read;
+            if ((read = _sampleSource.Read(buffer, 0, frameSize * frameCount)) > 0)
+            {
+                Dictionary<float, float> left = buffer.Where((value, index) => index % 2 == 0)
+                    .Select((x, i) => new { x, i }).ToDictionary(a => fromSec + (float)a.i / (float)_sampleSource.WaveFormat.SampleRate, a => a.x);
+                seriesLeft.ItemsSource = left;
+                Dictionary<float, float> right = buffer.Where((value, index) => index % 2 == 1)
+                    .Select((x, i) => new { x, i }).ToDictionary(a => fromSec + (float)a.i / (float)_sampleSource.WaveFormat.SampleRate, a => a.x);
+                seriesRight.ItemsSource = right;
+            }
+        }
+        void showVolume()
+        {
+            int frameSize = (int)iudFrameSize.Value;
+            int frameCount = (int)iudFrameCount.Value * 2;
+            float[] buffer = new float[frameSize];
+            Dictionary<double, double> dict = new Dictionary<double, double>();
+            for (int f = 0; f < frameCount; f++)
+            {
+                int read;
+                double fromSec = _sampleSource.GetPosition().TotalSeconds;
+                if ((read = _sampleSource.Read(buffer, 0, frameSize)) > 0)
+                {
+                    float[] left = buffer.Where((v, i) => i % 2 == 0).ToArray();
+                    float[] right = buffer.Where((v, i) => i % 2 == 1).ToArray();
+
+                    double normL = Math.Sqrt(left.Zip(left, (i, j) => i * j).Sum());
+                    double normR = Math.Sqrt(right.Zip(right, (i, j) => i * j).Sum());
+                    dict.Add(fromSec, normL+normR);
+                }
+            }
+            seriesLeft.ItemsSource = dict;
+            Dictionary<double, double> acDict = new Dictionary<double, double>();
+            double norm = dict.Zip(dict, (i, j) => i.Value * j.Value).Sum();
+            for (int n = 1; n < 1000; n++)
+            {
+                double inp = dict.Zip(dict.Skip(n), (i, j) => i.Value * j.Value).Sum();
+                double tm = (double)n * (double)frameSize / (double)_sampleSource.WaveFormat.SampleRate;
+                acDict.Add(tm, inp/norm);
+            }
+            seriesRight.ItemsSource = acDict;
         }
 
         private void btnShowWave_Click(object sender, RoutedEventArgs e)
         {
-            WrapTrack wt = lvTracks.SelectedItem as WrapTrack;
-            IITFileOrCDTrack track = wt.Track as IITFileOrCDTrack;
-            double fromSec = (double)iudFromSec.Value;
-            double toSec = (double)iudToSec.Value;
-            IWaveSource waveSource = CodecFactory.Instance.GetCodec(track.Location);
-            txWaveInfo.Text = string.Format("Rate:{0}\nChannels:{1}\nByte/Block:{2}\nByte/Sample:{3}",
-                waveSource.WaveFormat.SampleRate,
-                waveSource.WaveFormat.Channels,
-                waveSource.WaveFormat.BytesPerBlock,
-                waveSource.WaveFormat.BytesPerSample);
-            ISampleSource sampleSource = waveSource.ToSampleSource();
-            int sizeToRead = (int)(sampleSource.WaveFormat.SampleRate * (toSec - fromSec) * sampleSource.WaveFormat.Channels);
-            sampleSource.Position = (int)(sampleSource.WaveFormat.SampleRate * fromSec * sampleSource.WaveFormat.Channels);
+//            showWave();
+            showVolume();
+        }
 
-            int read = 0;
-            float[] buffer = new float[sampleSource.WaveFormat.Channels];
-            List<KeyValuePair<float, float>> left = new List<KeyValuePair<float, float>>();
-            List<KeyValuePair<float, float>> right = new List<KeyValuePair<float, float>>();
-            while ((read += sampleSource.Read(buffer, 0, sampleSource.WaveFormat.Channels)) < sizeToRead)
+        private void iudFromSec_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (this.IsInitialized)
             {
-                float pos = (float)sampleSource.Position / (float)(2 * sampleSource.WaveFormat.SampleRate);
-                left.Add(new KeyValuePair<float, float>(pos, buffer[0]));
-                if (sampleSource.WaveFormat.Channels > 1)
-                {
-                    right.Add(new KeyValuePair<float, float>(pos, buffer[1]));
-                }
+                showWave();
             }
-            
-            seriesLeft.ItemsSource = left;
-            seriesRight.ItemsSource = right;
+        }
+
+        private void iudToSec_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
         }
     }
 }
