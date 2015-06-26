b4033d5c59d51163823678cf0c8fe1086363afc1
xiaomuc <xkogij@gmail.com>
2015-06-26 17:30:52 +0900

    ハミングがうまいこといかないぜ

--- Labo/MainWindow.xaml.cs
+++ Labo/MainWindow.xaml.cs
@@ -52,11 +52,12 @@ namespace Labo
                      waveSource.WaveFormat.Channels);
             iudFrameCount.Value = (int)_sampleSource.Length / (int)iudFrameSize.Value;
             btnShowWave.IsEnabled = true;
+            showVolume();
         }
         void showWave()
         {
             int frameSize = (int)iudFrameSize.Value;
-            int frameCount = (int)iudFrameCount.Value*2;
+            int frameCount = (int)iudFrameCount.Value * 2;
             float[] buffer = new float[frameSize * frameCount];
             float fromSec = (float)_sampleSource.GetPosition().TotalSeconds;
             int read;
@@ -64,10 +65,10 @@ namespace Labo
             {
                 Dictionary<float, float> left = buffer.Where((value, index) => index % 2 == 0)
                     .Select((x, i) => new { x, i }).ToDictionary(a => fromSec + (float)a.i / (float)_sampleSource.WaveFormat.SampleRate, a => a.x);
-                seriesLeft.ItemsSource = left;
+                seriesVolume.ItemsSource = left;
                 Dictionary<float, float> right = buffer.Where((value, index) => index % 2 == 1)
                     .Select((x, i) => new { x, i }).ToDictionary(a => fromSec + (float)a.i / (float)_sampleSource.WaveFormat.SampleRate, a => a.x);
-                seriesRight.ItemsSource = right;
+                seriesVolAc.ItemsSource = right;
             }
         }
         void showVolume()
@@ -75,39 +76,92 @@ namespace Labo
             int frameSize = (int)iudFrameSize.Value;
             int frameCount = (int)iudFrameCount.Value * 2;
             float[] buffer = new float[frameSize];
-            Dictionary<double, double> dict = new Dictionary<double, double>();
+            Dictionary<double, double> vol = new Dictionary<double, double>();
+            Dictionary<double, double> diff = new Dictionary<double, double>();
+            double s = (double)_sampleSource.WaveFormat.SampleRate / (double)frameSize;
+            double volPrev = 0;
             for (int f = 0; f < frameCount; f++)
             {
                 int read;
-                double fromSec = _sampleSource.GetPosition().TotalSeconds;
+                double tm = (double)f / s;
                 if ((read = _sampleSource.Read(buffer, 0, frameSize)) > 0)
                 {
                     float[] left = buffer.Where((v, i) => i % 2 == 0).ToArray();
                     float[] right = buffer.Where((v, i) => i % 2 == 1).ToArray();
 
-                    double normL = Math.Sqrt(left.Zip(left, (i, j) => i * j).Sum());
-                    double normR = Math.Sqrt(right.Zip(right, (i, j) => i * j).Sum());
-                    dict.Add(fromSec, normL+normR);
+                    double normL = Math.Sqrt(left.Zip(left, (i, j) => i * j).Sum() / (double)frameSize);
+                    double normR = Math.Sqrt(right.Zip(right, (i, j) => i * j).Sum() / (double)frameSize);
+                    double volNow = normL + normR;
+                    vol.Add(tm, volNow);
+                    if (volNow > volPrev)
+                    {
+                        diff.Add(tm, volNow - volPrev);
+                    }
+                    else
+                    {
+                        diff.Add(tm, 0);
+                    }
+                    volPrev = volNow;
                 }
             }
-            seriesLeft.ItemsSource = dict;
-            Dictionary<double, double> acDict = new Dictionary<double, double>();
-            double norm = dict.Zip(dict, (i, j) => i.Value * j.Value).Sum();
+            seriesVolume.ItemsSource = vol;
+            Dictionary<double, double> volAc = new Dictionary<double, double>();
+            double norm = vol.Zip(vol, (i, j) => i.Value * j.Value).Sum();
             for (int n = 1; n < 1000; n++)
             {
-                double inp = dict.Zip(dict.Skip(n), (i, j) => i.Value * j.Value).Sum();
+                double inp = vol.Zip(vol.Skip(n), (i, j) => i.Value * j.Value).Sum();
                 double tm = (double)n * (double)frameSize / (double)_sampleSource.WaveFormat.SampleRate;
-                acDict.Add(tm, inp/norm);
+                volAc.Add(tm, inp / norm);
             }
-            seriesRight.ItemsSource = acDict;
-            BpmDetector detector = new BpmDetector(new BPMDetectorConfig());
-            Dictionary<int,double>bpmList= detector.calculateBPM(acDict, _sampleSource.WaveFormat.SampleRate, frameSize);
-            seriesBPM.ItemsSource = bpmList;
+            Dictionary<double, double> volAcLinear = calcAB(volAc);
+            seriesVolAc.ItemsSource = volAc;
+            //            BpmDetector detector = new BpmDetector(new BPMDetectorConfig());
+            //            Dictionary<int, double> bpmList = detector.calculateBPM(diff, _sampleSource.WaveFormat.SampleRate, frameSize);
+            seriesVolLinear.ItemsSource = volAcLinear;
+            Dictionary<double, double> volAcNorm = volAc.Zip(volAcLinear, (i, j) => new { Key = i.Key, Value = i.Value - j.Value }).ToDictionary(x => x.Key, x => x.Value);
+            seriesVolAcNorm.ItemsSource = volAcNorm;
+
+            //            Dictionary<double, double> volHam = volAc.Zip(getHamming(volAc.Count), (i, j) => new { Key = i.Key, Value = i.Value * j }).ToDictionary(x => x.Key, x => x.Value);
+            Dictionary<double, double> volHam = volAc.Select((x, i) => new { x, i }).ToDictionary(y => y.x.Key, y => getHamming(y.x.Value, y.i, volAc.Count));
+            seriesVolAcHam.ItemsSource = volHam;
+        }
+        double getHamming(double value, int index, int count)
+        {
+            double h = getHamming(index, count);
+            double ret = value * h;
+            return ret;
+        }
+        double getHamming(int index, int count)
+        {
+            return 0.54 - 0.46 * Math.Cos(2 * Math.PI * (double)index / (double)(count - 1));
+        }
+        List<double> getHamming(int count)
+        {
+            List<double> list = new List<double>();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(0.54 - 0.46 * Math.Cos(2 * Math.PI * (double)i / (double)(count - 1)));
+            }
+            return list;
+        }
+
+        Dictionary<double, double> calcAB(Dictionary<double, double> data)
+        {
+            double n = (double)data.Count;
+            //
+            double P = data.Sum(d => d.Key);
+            double Q = data.Sum(d => d.Value);
+            double R = data.Sum(d => d.Key * d.Value);
+            double S = data.Sum(d => d.Key * d.Key);
+
+            double a = (n * R - P * Q) / (n * S - P * P);
+            double b = (S * Q - R * P) / (n * S - P * P);
+            return data.ToDictionary(x => x.Key, x => x.Key * a + b);
         }
 
         private void btnShowWave_Click(object sender, RoutedEventArgs e)
         {
-//            showWave();
+            //            showWave();
             showVolume();
         }
 
